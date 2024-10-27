6c8a70872c74c290fee558f169e5aca71c9585ce
Alisson Livio <livilia74629@gmail.com>
2024-10-27 17:17:25 -0300

    criaçãos das classes principais

---
+++ Hospital_Project/src/modelo/Consulta.java
@@ -0,0 +1,105 @@
+package modelo;
+
+public class Consulta {
+	
+	private String data;
+	private String hora;
+	private Medico medico;
+	private Paciente paciente;
+	private String queixaPaciente;
+	private String tipoConsulta;
+	private String convenio;
+	private String observacoes;
+	private Material materiais;
+	
+	public Consulta() {
+		
+	}
+	
+	public Consulta(String data, String hora, Medico medico, Paciente paciente, String queixaPaciente,
+					String tipoConsulta, String convenio, String observacoes, Material materiais) {
+		this.data = data;
+		this.hora = hora;
+		this.medico = medico;
+		this.paciente = paciente;
+		this.queixaPaciente = queixaPaciente;
+		this.convenio = convenio;
+		this.observacoes = observacoes;
+		this.materiais = materiais;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public String getQueixaPaciente() {
+		return queixaPaciente;
+	}
+
+	public void setQueixaPaciente(String queixaPaciente) {
+		this.queixaPaciente = queixaPaciente;
+	}
+
+	public String getTipoConsulta() {
+		return tipoConsulta;
+	}
+
+	public void setTipoConsulta(String tipoConsulta) {
+		this.tipoConsulta = tipoConsulta;
+	}
+
+	public String getConvenio() {
+		return convenio;
+	}
+
+	public void setConvenio(String convenio) {
+		this.convenio = convenio;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Material getMateriais() {
+		return materiais;
+	}
+
+	public void setMateriais(Material materiais) {
+		this.materiais = materiais;
+	}
+	
+	
+
+}
---
+++ Hospital_Project/src/modelo/Exame.java
@@ -0,0 +1,79 @@
+package modelo;
+
+public class Exame {
+	
+	private String nomeExame;
+	private String descricao;
+	private String tipoExame;
+	private float valorParticular;
+	private Material materiais;
+	private Medico medico;
+	
+	
+	
+	public Exame() {
+		
+	}
+	
+	public Exame(String nomeExame, String descricao, String tipoExame, float valorParticular, Material materiais, Medico medico) {
+		this.nomeExame = nomeExame;
+		this.descricao = descricao;
+		this.tipoExame = tipoExame;
+		this.valorParticular = valorParticular;
+		this.materiais = materiais;
+		this.medico = medico;
+		
+		
+	}
+
+	public String getNomeExame() {
+		return nomeExame;
+	}
+
+	public void setNomeExame(String nomeExame) {
+		this.nomeExame = nomeExame;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipoExame() {
+		return tipoExame;
+	}
+
+	public void setTipoExame(String tipoExame) {
+		this.tipoExame = tipoExame;
+	}
+
+	public float getValorParticular() {
+		return valorParticular;
+	}
+
+	public void setValorParticular(float valorParticular) {
+		this.valorParticular = valorParticular;
+	}
+
+	public Material getMateriais() {
+		return materiais;
+	}
+
+	public void setMateriais(Material materiais) {
+		this.materiais = materiais;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	
+	
+
+}
