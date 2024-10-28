f4bbc09c3d8fcf17e4b21cf188c53aefd722dc6e
Alisson Livio <livilia74629@gmail.com>
2024-10-27 23:43:46 -0300

    controlador da interface gráfica do cadastro de materiais

---
+++ Hospital_Project/src/controlador/ControladorCadastrarMaterial.java
@@ -0,0 +1,24 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import visual.PanelCadastrarMaterial;
+
+public class ControladorCadastrarMaterial implements ActionListener {
+	
+	PanelCadastrarMaterial panelCadastrarMaterial;
+	
+	
+	public ControladorCadastrarMaterial(PanelCadastrarMaterial panelCadastrarMaterial) {
+		this.panelCadastrarMaterial = panelCadastrarMaterial;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
