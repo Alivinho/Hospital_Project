abab3efa2b72996b8bb3d2d0932dc128b7ec2af1
Alisson Livio <livilia74629@gmail.com>
2024-10-27 23:44:36 -0300

    controaldor da interface gráfica do cadastro dos médicos

---
+++ Hospital_Project/src/controlador/ControladorCadastrarMedico.java
@@ -0,0 +1,34 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import visual.PanelCadastrarMedico;
+
+public class ControladorCadastrarMedico implements ActionListener{
+	
+	PanelCadastrarMedico panelCadastrarMedico;
+	
+	
+	public ControladorCadastrarMedico(PanelCadastrarMedico panelCadastrarMedico) {
+		this.panelCadastrarMedico = panelCadastrarMedico; 
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
