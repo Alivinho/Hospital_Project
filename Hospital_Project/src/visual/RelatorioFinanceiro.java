ba835ec83838da51d8ec9f4a063d2e827ad75565
Mateus Santos <mateussilvanzg@gmail.com>
2024-11-10 17:34:10 -0300

    Painéis de relatório adicionados (falta preencher)

---
+++ Hospital_Project/src/visual/RelatorioFinanceiro.java
@@ -0,0 +1,14 @@
+package visual;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class RelatorioFinanceiro extends JPanel {
+	public RelatorioFinanceiro() {
+		setBackground(new Color(89, 169, 255));
+		this.setSize(960, 790);
+		setLayout(null);
+	}
+
+}
