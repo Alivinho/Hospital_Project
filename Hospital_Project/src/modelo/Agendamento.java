65720fa0d07b8ff0d003dd1b990bac3cc7824b09
Alisson Livio <livilia74629@gmail.com>
2024-11-12 23:02:18 -0300

    atualizando a classe agendamento

--- Hospital_Project/src/modelo/Agendamento.java
+++ Hospital_Project/src/modelo/Agendamento.java
@@ -1,5 +1,72 @@
 package modelo;
 
+import java.util.Date;
+
 public class Agendamento {
+	
+	private String medico;
+	private String paciente;
+	private String data;
+	private String hora;
+	private String tipoExame;
+	
+	public Agendamento() {
+		
+	}
+	
+	public Agendamento(String medico, String paciente, String data, String hora, String tipoExame) {
+		this.medico = medico;
+		this.paciente = paciente;
+		this.data = data;
+		this.hora = hora;
+		this.tipoExame = tipoExame;
+		
+	}
+
+	public String getMedico() {
+		return medico;
+	}
+
+	public void setMedico(String medico) {
+		this.medico = medico;
+	}
+
+	public String getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(String paciente) {
+		this.paciente = paciente;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public String getTipoExame() {
+		return tipoExame;
+	}
+
+	public void setTipoExame(String tipoExame) {
+		this.tipoExame = tipoExame;
+	}
+	
+	
+	
+	
+	
+	
 
 }
