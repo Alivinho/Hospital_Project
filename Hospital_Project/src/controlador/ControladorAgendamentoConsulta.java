6714f087bf44d4a2d76abc9622371d86036b0039
Alisson Livio <livilia74629@gmail.com>
2024-11-12 23:02:49 -0300

    tratamento de exceções do agendamento de consulta e exame

--- Hospital_Project/src/controlador/ControladorAgendamentoConsulta.java
+++ Hospital_Project/src/controlador/ControladorAgendamentoConsulta.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import modelo.Agendamento;
 import visual.PanelAgendamentoConsulta;
 
@@ -18,13 +20,66 @@ public class ControladorAgendamentoConsulta implements ActionListener {
 	}
 	
 	private void addEventos() {
-		// TODO Auto-generated method stub
+		panelAgendamentoConsulta.getBtnAgendar().addActionListener(this);  
+		panelAgendamentoConsulta.getBtnLimpar().addActionListener(this);  
 		
 	}
 
-	@Override
+	
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getSource() == panelAgendamentoConsulta.getBtnAgendar()) {
+			agendarConsulta();
+		}else if(e.getSource() == panelAgendamentoConsulta.getBtnLimpar()){
+			limparCampos();
+		}	
+		
+	}
+
+	
+
+	public void agendarConsulta() {
+		try {
+			
+			String medico = panelAgendamentoConsulta.getTextFieldMedico().getText().trim();
+			String paciente = panelAgendamentoConsulta.getTextFieldPaciente().getText().trim();
+			String data = panelAgendamentoConsulta.getTextFieldData().getText().trim();
+			String hora = panelAgendamentoConsulta.getTextFieldHorario().getText().trim();
+			
+			if (medico.isEmpty() || paciente.isEmpty() || data.isEmpty() || hora.isEmpty()) {
+				throw new IllegalArgumentException("Todos os campos devem ser preenchidos.");
+			}
+			
+			
+			if (!data.matches("\\d{2}/\\d{2}/\\d{4}") || !hora.matches("\\d{2}:\\d{2}")) {
+				throw new IllegalArgumentException("Data ou horário em formato inválido.");
+			}
+			
+			if (!medico.matches("^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ0-9\\s]*$")) {
+	                throw new Exception("O nome do material deve conter apenas letras e espaços.");
+	           }
+	            
+	        if (!paciente.matches("^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ0-9\\s]*$")) {
+	                throw new Exception("A especialidade deve conter apenas letras e espaços.");
+	           }
+	        
+	        Agendamento agendamento = new Agendamento(data, hora, medico, paciente, null);
+	        consultasAgendadas.add(agendamento);
+	        
+	        JOptionPane.showMessageDialog(panelAgendamentoConsulta, "[SUCESSO ✅ ]: Consulta agendada com sucesso!", "Sucesso!", JOptionPane.INFORMATION_MESSAGE);
+            limparCampos();
+			
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(panelAgendamentoConsulta, "[ERRO ❌ ]: " + e.getMessage(), "Erro", JOptionPane.WARNING_MESSAGE);
+		}
+		
+	}
+	
+	public void limparCampos() {
+		panelAgendamentoConsulta.getTextFieldMedico().setText("");
+		panelAgendamentoConsulta.getTextFieldPaciente().setText("");
+		panelAgendamentoConsulta.getTextFieldHorario().setText("");
+		panelAgendamentoConsulta.getTextFieldData().setText("");
 		
 	}
 }
--- Hospital_Project/src/controlador/ControladorAgendamentoExame.java
+++ Hospital_Project/src/controlador/ControladorAgendamentoExame.java
@@ -2,20 +2,96 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
+import modelo.Agendamento;
 import visual.PanelAgendamentoExame;
 
 public class ControladorAgendamentoExame implements ActionListener {
-	PanelAgendamentoExame panelEAgendamentoExame;
+	PanelAgendamentoExame panelAgendamentoExame;
+	ArrayList<Agendamento> examesAgendados;
 	
 	public ControladorAgendamentoExame(PanelAgendamentoExame panelAgendamentoExame) {
-		this.panelEAgendamentoExame = panelAgendamentoExame;
+		this.panelAgendamentoExame = panelAgendamentoExame;
+		examesAgendados = new ArrayList<Agendamento>();
+		addEventos();
 	}
 	
-	@Override
+	private void addEventos() {
+		panelAgendamentoExame.getBtnAgendar().addActionListener(this);  
+		panelAgendamentoExame.getBtnLimpar().addActionListener(this);  		
+	}
+
+	
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getSource() == panelAgendamentoExame.getBtnAgendar()) {
+			agendarConsulta();
+		}else if(e.getSource() == panelAgendamentoExame.getBtnLimpar()){
+			limparCampos();
+		}		
+	}
+
+	
+	public void agendarConsulta() {
+		try {
+			
+			String paciente = panelAgendamentoExame.getTextFieldPaciente().getText().trim();
+			String tipoExame = (String) panelAgendamentoExame.getComboBoxTipoExame().getSelectedItem();
+			String data = panelAgendamentoExame.getTextFieldData().getText().trim();
+			String hora = panelAgendamentoExame.getTextField_1_1().getText().trim();
+			
+			
+			if (paciente.isEmpty() || tipoExame.isEmpty() || data.isEmpty() || hora.isEmpty()) {
+				throw new IllegalArgumentException("Todos os campos devem ser preenchidos.");
+			}
+			
+			
+			if (!data.matches("\\d{2}/\\d{2}/\\d{4}") || !hora.matches("\\d{2}:\\d{2}")) {
+				throw new IllegalArgumentException("Data ou horário em formato inválido.");
+			}
+			
+			 String[] dataParts = data.split("/");
+		        int anoInformado = Integer.parseInt(dataParts[2]);
+		        int anoAtual = java.time.Year.now().getValue();
+
+		        if (anoInformado < anoAtual) {
+		            throw new IllegalArgumentException("O ano da data não pode ser menor que o ano atual.");
+		        }
+
+			
+			if (!paciente.matches("^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ0-9\\s]*$")) {
+	                throw new Exception("O nome do material deve conter apenas letras e espaços.");
+	           }
+	            
+			if(panelAgendamentoExame.getComboBoxTipoExame().getSelectedIndex() == 0) {
+            	throw new Exception("Selecione um tipo de exame.");
+            }
+			
+			Agendamento agendamento = new Agendamento(null, paciente, tipoExame, data, hora);
+			examesAgendados.add(agendamento);
+			
+			 
+	        JOptionPane.showMessageDialog(panelAgendamentoExame, "[SUCESSO ✅ ]: Exame agendada com sucesso!", "Sucesso!", JOptionPane.INFORMATION_MESSAGE);
+            limparCampos();
+			
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(panelAgendamentoExame, "[ERRO ❌ ]: " + e.getMessage(), "Erro", JOptionPane.WARNING_MESSAGE);
+
+		}
 		
 	}
+	
+	public void limparCampos() {
+		panelAgendamentoExame.getTextField_1_1().setText("");
+		panelAgendamentoExame.getTextFieldPaciente().setText("");		
+		panelAgendamentoExame.getComboBoxTipoExame().setSelectedIndex(0);	
+		panelAgendamentoExame.getTextFieldData().setText("");		
+
+
+	}
+
 
 }
