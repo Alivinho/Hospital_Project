b508ef9126531b1e827630e2a2a49a18e7f10f3e
Alisson Livio <livilia74629@gmail.com>
2024-12-12 12:03:11 -0300

    teste

--- Hospital_Project/src/controlador/ControladorCadastrarMaterial.java
+++ Hospital_Project/src/controlador/ControladorCadastrarMaterial.java
@@ -6,6 +6,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
@@ -98,16 +100,27 @@ public class ControladorCadastrarMaterial implements ActionListener {
 	
 	private void gravarDados(Material material) {
 	    System.out.println("Diretório atual: " + System.getProperty("user.dir"));
+	    
+	    
+	    String resourcePath = "/dados/dadosMaterial.txt";
+	    URL resourceUrl = getClass().getResource(resourcePath);
 
-	    // Caminho ajustado para o local correto do arquivo dentro do projeto
-	    String filePath = "src/dados/dadosMaterial.txt"; 
-	    System.out.println("Tentando gravar no arquivo: " + new File(filePath).getAbsolutePath());
-
-	    // Criação do diretório 'dados' caso não exista
-	    File file = new File(filePath);
+	    if (resourceUrl == null) {
+	        System.out.println("Arquivo não encontrado no classpath: " + resourcePath);
+	        return;
+	    }
+	    
+	    File file;
+	    try {
+	        file = new File(resourceUrl.toURI());
+	    } catch (URISyntaxException e) {
+	        System.out.println("Erro ao converter o caminho do recurso para URI:");
+	        e.printStackTrace();
+	        return;
+	    }
+	    
 	    File parentDirectory = file.getParentFile(); // Obtém o diretório pai
-
-	    /*if (!parentDirectory.exists()) {
+	    if (!parentDirectory.exists()) {
 	        boolean created = parentDirectory.mkdirs(); // Cria o diretório
 	        if (created) {
 	            System.out.println("Diretório 'dados' criado com sucesso!");
@@ -115,18 +128,19 @@ public class ControladorCadastrarMaterial implements ActionListener {
 	            System.out.println("Falha ao criar o diretório 'dados'. Verifique permissões.");
 	            return;
 	        }
-	    }*/
-
+	    }
+	    
 	    // Gravação no arquivo
 	    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
 	        writer.write(material.toString());
 	        writer.newLine();
-	        System.out.println("Dados gravados com sucesso em dadosPaciente.txt:");
+	        System.out.println("Dados gravados com sucesso em dadosMaterial.txt:");
 	        System.out.println(material.toString());
 	    } catch (IOException e) {
 	        System.out.println("Erro ao gravar os dados no arquivo:");
 	        e.printStackTrace();
 	    }
+	   
 	}
 
 	private void limparCampos() {
